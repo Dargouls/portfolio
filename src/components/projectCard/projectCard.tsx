a4f3eb1305cde0fb474cd627b80905af86c36cdd
Gabriel Azevedo <gah_500@hotmail.com>
2024-07-31 19:57:48 -0300

    fix: card image height

--- src/components/projectCard/projectCard.tsx
+++ src/components/projectCard/projectCard.tsx
@@ -14,7 +14,7 @@ export default function ProjectCard({ name, description, url, image }: ProjectCa
 			<div className='group flex flex-col gap-4 md:max-w-[300px] w-full bg-background-light border border-contrast rounded-xl overflow-hidden cursor-pointer'>
 				<div id='image-container' className='w-full overflow-hidden'>
 					<Image
-						className='group-hover:scale-110 group-active:scale-100 w-full brightness-50 blur-[2px] group-hover:blur-0 group-hover:brightness-100 transition-all duration-700'
+						className='group-hover:scale-110 group-active:scale-100 w-full h-40 brightness-50 blur-[2px] group-hover:blur-0 group-hover:brightness-100 transition-all duration-700'
 						src={image}
 						alt={name}
 						height={200}
