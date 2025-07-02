28f3c9209133c0787a987a0075cf39b52b8f3388
Dargouls <gah_500@hotmail.com>
2025-07-02 11:54:03 -0300

    feat: add project

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -3,8 +3,10 @@ import { HTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 import ProjectCard from '../projectCard/projectCard';
 
+import artiux from '@/assets/images/artiux.png';
 import chatLlama from '@/assets/images/chat-llama.png';
 import countplus from '@/assets/images/count-plus.png';
+import guerreiros from '@/assets/images/guerreiros.png';
 import iris from '@/assets/images/iris-analyzer.jpeg';
 
 export default function Projects(props: HTMLAttributes<HTMLDivElement>) {
@@ -37,7 +39,13 @@ export default function Projects(props: HTMLAttributes<HTMLDivElement>) {
 							name='Guerreiros Naventura'
 							description='Institucional para a organização Guerreiros Naventura / Corrida e Trekking'
 							url='https://www.orientextreme.com.br/'
-							image={iris.src}
+							image={guerreiros.src}
+						/>
+						<ProjectCard
+							name='artIux Lab'
+							description='Biblioteca código aberto de componentes focada em animações e efeitos visuais'
+							url='https://artiux.vercel.app/'
+							image={artiux.src}
 						/>
 					</div>
 				</div>
